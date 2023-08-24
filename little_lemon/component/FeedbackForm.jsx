4cb0514a66d15e95932a13db0cdcbc4d87f4215a
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-24 09:57:28 +0100

    input field enhancement

--- little_lemon/component/FeedbackForm.jsx
+++ little_lemon/component/FeedbackForm.jsx
@@ -1,6 +1,6 @@
 
 import React, { useState } from "react";
-import { ScrollView, StyleSheet, Text, TextInput } from "react-native";
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TextInput } from "react-native";
 
 const FeedbackForm = () => {
   // declare the variables
@@ -10,32 +10,51 @@ const FeedbackForm = () => {
   const [phoneNumber, onChangePhoneNumber] = useState("");
 
   return (
-    <ScrollView style={styles.container}>
+    <KeyboardAvoidingView
+   style={styles.container}
+   behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+
+    <ScrollView style={styles.container} keyboardDismissMode="on-drag" >
       <Text style={styles.headingSection}>
-        How was your visit to Little Lemon?      {" "}
+        How was your visit to Little Lemon?{" "}
       </Text>
       <Text style={styles.infoSection}>
-                Little Lemon is a charming neighborhood bistro that serves
-        simple food         and classic cocktails in a lively but casual
-        environment. We would love         to hear your experience with us!    
-         {" "}
+        Little Lemon is a charming neighborhood bistro that serves
+        simple food and classic cocktails in a lively but casual
+        environment. We would love to hear your experience with us!
       </Text>
       <TextInput
         style={styles.input}
         value={firstName}
         onChangeText={onChangeFirstName}
+        placeholder="enter your first name"
+        autoCorrect={true}
+        autoCapitalize="sentences"
       />
       <TextInput
         style={styles.input}
         value={lastName}
         onChangeText={onChangeLastName}
+        placeholder="enter your last name"
+      />
+      <TextInput
+        style={styles.input}
+        value={phoneNumber}
+        onChangeText={onChangePhoneNumber}
+        placeholder="enter your phone number"
+        maxLength={14}
+        keyboardType="phone-pad"
       />
       <TextInput
         style={styles.messageInput}
         value={message}
         onChangeText={onChangeMessage}
+        placeholder="enter your message"
+        multiline
+        maxLength={250}
       />
     </ScrollView>
+   </KeyboardAvoidingView>
   );
 };
 
@@ -59,7 +78,7 @@ const styles = StyleSheet.create({
     padding: 10,
     fontSize: 16,
     backgroundColor: "#F4CE14",
-    textAlign:'center',
+    // textAlign:'center',
     // wordWrap: 'break-word',
   },
   infoSection: {
