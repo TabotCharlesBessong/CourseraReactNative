1f7eabba262bf21511f8570d03de097ff0c994e3
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-08-26 13:27:31 +0100

    bottom tab navigation

--- little_lemon/navigation/BottomTabNavigation.jsx
+++ little_lemon/navigation/BottomTabNavigation.jsx
@@ -1,11 +1,23 @@
 
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { SectionA,SectionLists,MenuItem } from '../component'
+import { NavigationContainer } from '@react-navigation/native'
+
+const Tab = createBottomTabNavigator()
 
 const BottomTabNavigation = () => {
   return (
-    <View>
-      <Text>BottomTabNavigation</Text>
+    <View  >
+      <Tab.Navigator screenOptions={{
+        bottom:0,
+        // posi 
+      }} >
+        <Tab.Screen name='sectionA' component={SectionA}  />
+        <Tab.Screen name='sectionL' component={SectionLists}  />
+        <Tab.Screen name='MenuI' component={MenuItem}  />
+      </Tab.Navigator>
     </View>
   )
 }
